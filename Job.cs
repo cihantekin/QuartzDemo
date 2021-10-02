49b772368410bafcaafa268fa5e09c479d01e381
Cihan Tekin <cihantekin1990@gmail.com>
2021-10-03 01:41:41 +0300

    quartzdemo

---
+++ Job.cs
@@ -0,0 +1,45 @@
+﻿using Quartz;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace QuartzDemo
+{
+    public class Job : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            bool result = false;
+
+            try
+            {
+                MethodInfo method = typeof(Job).GetMethod(context.JobDetail.Key.Name);
+
+                if (method != null)
+                {
+                    result = true;
+                    var r = method.Invoke(this, Array.Empty<object>());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return Task.FromResult(result);
+        }
+
+        public void Monthly()
+        {
+            System.Diagnostics.Debug.WriteLine("1. Job Başlatıldı");
+        }
+        public void Weekly()
+        {
+            System.Diagnostics.Debug.WriteLine("2. Job Başlatıldı");
+        }
+        public void Daily()
+        {
+            System.Diagnostics.Debug.WriteLine("3. Job Başlatıldı");
+        }
+    }
+}
---
+++ JobSchedulerService.cs
@@ -0,0 +1,71 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Threading.Tasks;
+
+namespace QuartzDemo
+{
+    public class JobSchedulerService
+    {
+        public async Task StartAsync()
+        {
+            try
+            {
+                var schedulerContext = new StdSchedulerFactory();
+                var scheduler = await schedulerContext.GetScheduler();
+
+                if (!scheduler.IsStarted)
+                {
+                    await scheduler.Start();
+                }
+
+                await CreateMonthlyJob(scheduler);
+                await CreateWeeklyJob(scheduler);
+                await CreateDailyJob(scheduler);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private async Task CreateMonthlyJob(IScheduler scheduler)
+        {
+            IJobDetail job = JobBuilder.Create<Job>().WithIdentity("Monthly", "group1").Build();
+
+            ITrigger trigger = TriggerBuilder.Create().WithSchedule(CronScheduleBuilder.
+                MonthlyOnDayAndHourAndMinute(1, 1, 1).
+                InTimeZone(TimeZoneInfo.Local)).
+                WithDescription("Monthle Job").
+                Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+        }
+
+        private async Task CreateWeeklyJob(IScheduler scheduler)
+        {
+            IJobDetail job = JobBuilder.Create<Job>().WithIdentity("Weekly", "group1").Build();
+
+            ITrigger trigger = TriggerBuilder.Create().WithSchedule(CronScheduleBuilder
+                .WeeklyOnDayAndHourAndMinute(DayOfWeek.Monday, 1, 0)
+                .InTimeZone(TimeZoneInfo.Local))
+                .WithDescription("Weekly Job")
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+        }
+
+        private async Task CreateDailyJob(IScheduler scheduler)
+        {
+            IJobDetail job = JobBuilder.Create<Job>().WithIdentity("Daily", "group1").Build();
+
+            ITrigger trigger = TriggerBuilder.Create().WithSchedule(CronScheduleBuilder
+                .DailyAtHourAndMinute(1, 34)
+                .InTimeZone(TimeZoneInfo.Local))
+                .WithDescription("Daily Job")
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
